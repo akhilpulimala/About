63e9dfc209483377b8345781b6a0268ecf9199ff
akhilpulimala <64695974+akhilpulimala@users.noreply.github.com>
2024-08-25 00:49:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
 from typing import Union
 
-from fastapi import FastAPI, Response, Request
-from fastapi.responses import HTMLResponse
-from schemas import Skills, LinkResponse
+from fastapi import Body, FastAPI, Response, Request
+from fastapi.responses import HTMLResponse, PlainTextResponse
+from schemas import Skills, LinkResponse, MessageBody
 app = FastAPI()
 
 
@@ -29,6 +29,23 @@ def about():
 def skills():
     return Skills.skills
 
-@app.get("/onlineme", response_model=LinkResponse)
+@app.get("/online-me")
 def social():
-    return {"link": "https://www.linkedin.com/in/akhil-pulimala/"}
+    return {"Linkedin": "https://www.linkedin.com/in/akhil-pulimala/",
+            "github": "https://github.com/akhilpulimala"}
+
+@app.get("/resume", response_model=LinkResponse)
+def view_resume():
+    return {"link": "https://yellow-stacia-24.tiiny.site"}
+
+@app.get("/contact-me")
+def contact_me():
+    return {"mobile": "8129490391",
+            "email": "akhilpulimala@gmail.com"}
+
+@app.post("/text-me/")
+async def text_me_now(your_name: str,
+                      number: str,
+                      content: MessageBody):
+    
+    return {f"Hi {your_name}, Thanks for reaching out , Will getback you soon"}
